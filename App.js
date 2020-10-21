a9da385cdf8482393ed5df3421a0476a92e125cb
dhudo986 <dhudo986@mtroyal.ca>
2020-10-21 13:20:46 -0600

    GPA Calculator with eslint

--- App.js
+++ App.js
@@ -1,15 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+// Was uncertain how to split the components between Courses and Calculate
+import React, { useState } from 'react';
+import {
+  StyleSheet, Text, View, TextInput, Button,
+} from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
@@ -19,3 +12,107 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+// Input sanitizer from Add-Calculator in Course Content
+const inputSanitizer = (text, setter) => {
+  if (text.length === 0 || Number.isNaN(text)) {
+    setter(0);
+    return 0;
+  }
+  return parseFloat(text);
+};
+
+const Courses = () => {
+  const [num1, setNum1] = useState(0);
+  const [num2, setNum2] = useState(0);
+  const [num3, setNum3] = useState(0);
+  const [average, setAverage] = useState(0);
+
+  return (
+    <View style={styles.container}>
+      <Text
+        style={{
+          fontWeight: 'bold',
+          fontSize: 30,
+        }}
+      >
+        Grade Calculator
+      </Text>
+      <Text>Course 1</Text>
+      <TextInput
+        style={{
+          height: 40,
+          borderColor: 'black',
+          borderWidth: 1,
+          width: 130,
+          marginBottom: 5,
+          paddingLeft: 5,
+          borderRadius: 5,
+        }}
+        value={num1.toString()}
+        onChangeText={
+          (text) => {
+            setNum1(text);
+          }
+        }
+      />
+      <Text>Course 3</Text>
+      <TextInput
+        style={{
+          height: 40,
+          borderColor: 'black',
+          borderWidth: 1,
+          width: 130,
+          marginBottom: 5,
+          paddingLeft: 5,
+          borderRadius: 5,
+        }}
+        value={num2.toString()}
+        onChangeText={
+          (text) => {
+            setNum2(text);
+          }
+        }
+      />
+      <Text>Course 3</Text>
+      <TextInput
+        style={{
+          height: 40,
+          borderColor: 'black',
+          borderWidth: 1,
+          width: 130,
+          marginBottom: 5,
+          paddingLeft: 5,
+          borderRadius: 5,
+        }}
+        value={num3.toString()}
+        onChangeText={
+          (text) => {
+            setNum3(text);
+          }
+        }
+      />
+      <Button
+        onPress={() => {
+          setAverage((inputSanitizer(num1, setNum1)
+          + inputSanitizer(num2, setNum2)
+          + inputSanitizer(num3, setNum3)) / 3);
+        }}
+        title="Average"
+      />
+      <Text style={{ padding: 20 }}>
+        Your average is:
+        {' '}
+        {+average.toFixed(2)}
+      </Text>
+    </View>
+  );
+};
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Courses />
+    </View>
+  );
+}
